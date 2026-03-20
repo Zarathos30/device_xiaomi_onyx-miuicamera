76305e4998a609f2dc2ea79e1b5e7943727055aa
k4lchakra <k4lchakra@gmail.com>
2026-03-20 10:45:28 +0545

    onyx-miuicamera: change libgui-xiaomi graphic dependent from v6 to v7

--- extract-files.py
+++ extract-files.py
@@ -33,7 +33,7 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system/lib64/libgui-xiaomi.so': blob_fixup()
         .fix_soname()
-        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
+        .replace_needed('android.hardware.graphics.common-V6-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
     (
         'system/lib64/libcamera_algoup_jni.xiaomi.so',
         'system/lib64/libcamera_mianode_jni.xiaomi.so',
